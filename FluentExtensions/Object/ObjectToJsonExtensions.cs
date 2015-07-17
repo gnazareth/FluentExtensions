778f19fc38b32c96a4239711f437d09a762a2298
gnazareth <gerzon@gmail.com>
2015-07-17 11:08:27 -0600

    Documented object ToJson extension

--- FluentExtensions/Object/ObjectToJsonExtensions.cs
+++ FluentExtensions/Object/ObjectToJsonExtensions.cs
@@ -2,18 +2,36 @@
 
 namespace FluentExtensions.Object
 {
+    /// <summary>
+    /// Extensions on any object to serialize it into json format
+    /// </summary>
     public static class ObjectToJsonExtensions
     {
+        /// <summary>
+        /// Serializes an object to a Json string
+        /// </summary>
+        /// <param name="value">object to serialize</param>
+        /// <returns>json string of serialized object</returns>
         public static string ToJson(this object value)
         {
             return JsonConvert.SerializeObject(value);
         }
 
+        /// <summary>
+        /// Serializes an object to a Json string with indentation
+        /// </summary>
+        /// <param name="value">object to serialize</param>
+        /// <returns>json string of serialized object</returns>
         public static string ToPrettyJson(this object value)
         {
             return JsonConvert.SerializeObject(value, Formatting.Indented);
         }
 
+        /// <summary>
+        /// Serializes an object to a Json string preserving references
+        /// </summary>
+        /// <param name="value">object to serialize</param>
+        /// <returns>json string of serialized object</returns>
         public static string ToJsonPreservingReferences(this object value)
         {
             return JsonConvert.SerializeObject(value, Formatting.None, new JsonSerializerSettings
@@ -22,6 +40,12 @@ namespace FluentExtensions.Object
             });
         }
 
+
+        /// <summary>
+        /// Serializes an object to a Json string with indentation and preserving references
+        /// </summary>
+        /// <param name="value">object to serialize</param>
+        /// <returns>json string of serialized object</returns>
         public static string ToPrettyJsonPreservingReferences(this object value)
         {
             return JsonConvert.SerializeObject(value, Formatting.Indented, new JsonSerializerSettings
