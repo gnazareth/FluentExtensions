ee437f554680afdcc7c161d74b908b888cf306cb
gnazareth <gerzon@gmail.com>
2015-07-17 11:54:24 -0600

    Added int.Times extension. Cleanup.

---
+++ FluentExtensions.Tests/Int/TimesExtensionTests.cs
@@ -0,0 +1,60 @@
+﻿using FluentAssertions;
+using FluentExtensions.Int;
+using NUnit.Framework;
+
+namespace FluentExtensions.Tests.Int
+{
+    [TestFixture]
+    public class TimesExtensionTests
+    {
+        [Test]
+        public void TestPositiveIntegerWithoutIndex()
+        {
+            var sum = 0;
+            5.Times(() => sum += 1);
+            sum.Should().Be(5);
+        }
+
+        [Test]
+        public void TestPositiveIntegerWithIndex()
+        {
+            var sum = 0;
+            5.Times(i => sum += i);
+            sum.Should().Be(10);
+        }
+
+        [Test]
+        public void TestZeroIntegerWithoutIndex()
+        {
+            var sum = 0;
+            0.Times(() => sum += 1);
+            sum.Should().Be(0);
+        }
+
+        [Test]
+        public void TestZeroIntegerWithIndex()
+        {
+            var sum = 0;
+            0.Times(i => sum += i);
+            sum.Should().Be(0);
+        }
+
+        [Test]
+        public void TestNegativeIntegerWithoutIndex()
+        {
+            var sum = 0;
+            const int count = -5;
+            count.Times(() => sum += 1);
+            sum.Should().Be(5);
+        }
+
+        [Test]
+        public void TestNegativeIntegerWithIndex()
+        {
+            var sum = 0;
+            const int count = -5;
+            count.Times(i => sum += i);
+            sum.Should().Be(-10);
+        }
+    }
+}
---
+++ FluentExtensions/Int/TimesExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FluentExtensions.Int
+{
+    /// <summary>
+    /// Performs an action based on the int value
+    /// </summary>
+    public static class TimesExtension
+    {
+        /// <summary>
+        /// Loops the specified action based on the int value.
+        /// </summary>
+        /// <param name="count">The count.</param>
+        /// <param name="action">The action.</param>
+        public static void Times(this int count, Action action)
+        {
+            if (count >= 0) for (var i = 0; i < count; i++) action();
+            else for (var i = 0; i > count; i--) action();
+        }
+
+        /// <summary>
+        /// Loops the specified action based on the int value. Passes the zero based count index.
+        /// </summary>
+        /// <param name="count">The count.</param>
+        /// <param name="action">The action.</param>
+        public static void Times(this int count, Action<int> action)
+        {
+            if (count >= 0) for (var i = 0; i < count; i++) action(i);
+            else for (var i = 0; i > count; i--) action(i);
+        }
+    }
+}
