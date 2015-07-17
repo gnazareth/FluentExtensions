32ed01faf6c163f092ed96788c7065060e579e67
gnazareth <gerzon@gmail.com>
2015-07-17 10:58:35 -0600

    Added object to json extensions

---
+++ FluentExtensions.Tests/Object/ObjectExtensionTests.cs
@@ -0,0 +1,69 @@
+﻿using FluentAssertions;
+using FluentExtensions.Object;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace FluentExtensions.Tests.Object
+{
+    [TestFixture]
+    public class ObjectToJsonExtensionTests
+    {
+        [Test]
+        public void TestObjectToJson()
+        {
+            var myObj = new
+            {
+                FirstName = "John",
+                LastName = "Doe"
+            };
+
+            var json = myObj.ToJson();
+            json.Should().Be(JsonConvert.SerializeObject(myObj));
+        }
+
+        [Test]
+        public void TestObjectToPrettyJson()
+        {
+            var myObj = new
+            {
+                FirstName = "John",
+                LastName = "Doe"
+            };
+
+            var json = myObj.ToPrettyJson();
+            json.Should().Be(JsonConvert.SerializeObject(myObj, Formatting.Indented));
+        }
+
+        [Test]
+        public void TestObjectToJsonPreservingReferences()
+        {
+            var myObj = new
+            {
+                FirstName = "John",
+                LastName = "Doe"
+            };
+
+            var json = myObj.ToJsonPreservingReferences();
+            json.Should().Be(JsonConvert.SerializeObject(myObj, Formatting.None, new JsonSerializerSettings
+            {
+                PreserveReferencesHandling = PreserveReferencesHandling.Objects
+            }));
+        }
+
+        [Test]
+        public void TestObjectToPrettyJsonPreservingReferences()
+        {
+            var myObj = new
+            {
+                FirstName = "John",
+                LastName = "Doe"
+            };
+
+            var json = myObj.ToPrettyJsonPreservingReferences();
+            json.Should().Be(JsonConvert.SerializeObject(myObj, Formatting.Indented, new JsonSerializerSettings
+            {
+                PreserveReferencesHandling = PreserveReferencesHandling.Objects
+            }));
+        }
+    }
+}
---
+++ FluentExtensions/Object/ObjectToJsonExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+
+namespace FluentExtensions.Object
+{
+    public static class ObjectToJsonExtensions
+    {
+        public static string ToJson(this object value)
+        {
+            return JsonConvert.SerializeObject(value);
+        }
+
+        public static string ToPrettyJson(this object value)
+        {
+            return JsonConvert.SerializeObject(value, Formatting.Indented);
+        }
+
+        public static string ToJsonPreservingReferences(this object value)
+        {
+            return JsonConvert.SerializeObject(value, Formatting.None, new JsonSerializerSettings
+            {
+                PreserveReferencesHandling = PreserveReferencesHandling.Objects
+            });
+        }
+
+        public static string ToPrettyJsonPreservingReferences(this object value)
+        {
+            return JsonConvert.SerializeObject(value, Formatting.Indented, new JsonSerializerSettings
+            {
+                PreserveReferencesHandling = PreserveReferencesHandling.Objects
+            });
+        }
+    }
+}
